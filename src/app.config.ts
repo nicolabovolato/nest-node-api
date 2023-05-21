0708d0e23da222d9af5d7097dec80e04b2115886
Nicola <nicola.bovolato@gmail.com>
2023-05-21 12:54:52 +0200

    add: openapi

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,13 +1,9 @@
-import { version } from '../package.json';
-
 export type Config = {
   port: number;
-  logLevel: string;
-  version: string;
+  openapi: boolean;
 };
 
 export default (): Config => ({
   port: Number(process.env.PORT || 80),
-  logLevel: process.env.LOG_LEVEL || 'info',
-  version,
+  openapi: !!process.env.EXPOSE_OPENAPI || process.env.NODE_ENV != 'production',
 });
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { Logger } from 'nestjs-pino';
 
@@ -21,11 +22,28 @@ async function bootstrap() {
   app.useLogger(app.get(Logger));
 
   const config = app.get<ConfigService<Config>>(ConfigService);
-  const port = config.getOrThrow('port');
-
-  await app.listen(config.getOrThrow('port'), '::');
-
   const logger = app.get<Logger>(Logger);
+
+  const openapi = config.getOrThrow('openapi', { infer: true });
+
+  if (openapi) {
+    SwaggerModule.setup(
+      'documentation',
+      app,
+      SwaggerModule.createDocument(
+        app,
+        new DocumentBuilder().setTitle('NestJS Service').build(),
+      ),
+    );
+  }
+
+  const port = config.getOrThrow('port', { infer: true });
+
+  await app.listen(port, '::');
+
   logger.log(`Listening on http://[::]:${port}`);
+  if (openapi) {
+    logger.log(`SwaggerUI hosted at http://[::]:${port}/documentation`);
+  }
 }
 bootstrap();
